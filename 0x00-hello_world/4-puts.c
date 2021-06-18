8b6b99ed3fb3ce8167ad905b12296e0e36738775
skaranja94 <skaranja94@gmail.com>
2021-06-17 23:28:50 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - This uses the puts function to print line to standard output
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  puts("\" Programming is like building a multilingual puzzle");
+  return (0);
+}
+  
